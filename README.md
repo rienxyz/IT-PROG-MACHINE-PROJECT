d07ff371e8c1d20c72f34defddf20a5ef5bb57a8
Atomic <anthony.namol.090105@gmail.com>
2026-07-12 23:39:22 +0800

    build: Add ideas for website structure and flow

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # IT-PROG-MACHINE-PROJECT
+
+WEBSTIE FLOW FOR DIFFERENT ROLES
+accessible by all
+- login
+
+by users (can only access users views?)
+- /patient-appointment
+- /patient-register
+- /patient-find-doctor
+
+by doctors (can access users views?)
+- /doctor-appointment
+
+by secretaries (can access users views?)
+- /secretary-dashboard
+- /secretary-appointment
+
+by admins (can access doctors, secretaries, and users views?)
+- /admin-dashboard
+- /user-management
+- /resource-management
+- /data-management
+- /financial-management
--- database.sql
+++ database.sql
@@ -1,2 +1,33 @@
 CREATE DATABASE 
--- Users (staff, patient, or admin) or admin/users lang
+-- users
+    -- user_id {auto-increment} PK
+    -- first_name
+    -- role = [patient, secretary, doctors, administrator]
+    -- account_status = [verified, pending, suspended]
+    -- activity_status = [active, inactive]
+    -- insurance = [maxicare, intellicare, medicard, ...]
+    -- insurance_status = [accepted, declined]
+    -- preferred_specialty
+
+-- doctors
+    -- doctor_id {auto-increement} PK
+    -- first_name
+    -- last_name
+    -- specialty
+
+-- appointment
+    -- appointment_id {auto-increment} PK
+    -- user_id [dapat patients lang pwede] FK
+    -- doctor_id FK
+    -- insurance_id FK
+    -- room_number
+    -- date (scheduled date)
+    -- time (scheduled time)
+    -- status = [confirmed, cancelled, no show, reschedules, declined, completed, ...]
+
+-- logs
+    -- log_id {auto-increment} PK
+    -- appointment_id FK
+    -- message
+    -- date (current date of log creation)
+    -- time (current time of log creation)
