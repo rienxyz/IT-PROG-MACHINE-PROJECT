79eb414971786ee5b4586ed329130e9ec1454c3c
Atomic <anthony.namol.090105@gmail.com>
2026-07-28 23:39:36 +0800

    build: Add starting database

--- database.sql
+++ database.sql
@@ -1,33 +1,73 @@
-CREATE DATABASE 
--- users
-    -- user_id {auto-increment} PK
-    -- first_name
-    -- role = [patient, secretary, doctors, administrator]
-    -- account_status = [verified, pending, suspended]
-    -- activity_status = [active, inactive]
-    -- insurance = [maxicare, intellicare, medicard, ...]
-    -- insurance_status = [accepted, declined]
-    -- preferred_specialty
+-- -----------------------------------------------------
+-- Schema appoiintment_db
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `appointment_db` DEFAULT CHARACTER
+SET
+    utf8;
 
--- doctors
-    -- doctor_id {auto-increement} PK
-    -- first_name
-    -- last_name
-    -- specialty
+USE `appointment_db`;
 
--- appointment
-    -- appointment_id {auto-increment} PK
-    -- user_id [dapat patients lang pwede] FK
-    -- doctor_id FK
-    -- insurance_id FK
-    -- room_number
-    -- date (scheduled date)
-    -- time (scheduled time)
-    -- status = [confirmed, cancelled, no show, reschedules, declined, completed, ...]
+-- -----------------------------------------------------
+-- Table `appointment_db`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `appointment_db`.`users` (
+    `user_id` INT NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(45) NULL,
+    `last_name` VARCHAR(45) NULL,
+    `role` ENUM ('patient', 'secretary', 'doctor', 'admin') NULL,
+    `account_status` ENUM ('active', 'inactive') NULL,
+    `insurance` VARCHAR(45) NULL,
+    `insurance_status` ENUM ('accepted', 'declined') NULL,
+    `preferred_specialty` VARCHAR(45) NULL,
+    PRIMARY KEY (`user_id`)
+) ENGINE = InnoDB;
 
--- logs
-    -- log_id {auto-increment} PK
-    -- appointment_id FK
-    -- message
-    -- date (current date of log creation)
-    -- time (current time of log creation)
+-- -----------------------------------------------------
+-- Table `appointment_db`.`doctors`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `appointment_db`.`doctors` (
+    `doctor_id` INT NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(45) NULL,
+    `last_name` VARCHAR(45) NULL,
+    `specialty` VARCHAR(45) NULL,
+    PRIMARY KEY (`doctor_id`)
+) ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `appointment_db`.`appointments`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `appointment_db`.`appointments` (
+    `appointment_id` INT NOT NULL AUTO_INCREMENT,
+    `user_id` INT NULL,
+    `doctor_id` INT NULL,
+    `insurance` VARCHAR(45) NULL,
+    `room_number` VARCHAR(45) NULL,
+    `date` DATE NULL,
+    `time` TIME NULL,
+    `status` ENUM (
+        'confirmed',
+        'cancelled',
+        'no show',
+        'rescheduled',
+        'declined',
+        'completed'
+    ) NULL,
+    PRIMARY KEY (`appointment_id`),
+    INDEX `user_id_idx` (`user_id` ASC) VISIBLE,
+    INDEX `doctor_id_idx` (`doctor_id` ASC) VISIBLE,
+    CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `appointment_db`.`users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    CONSTRAINT `doctor_id` FOREIGN KEY (`doctor_id`) REFERENCES `appointment_db`.`doctors` (`doctor_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `appointment_db`.`logs`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `appointment_db`.`logs` (
+    `log_id` INT NOT NULL AUTO_INCREMENT,
+    `appointment_id` INT NULL,
+    `content` TEXT (500) NULL,
+    `timestamp` TIMESTAMP(3) NULL,
+    PRIMARY KEY (`log_id`),
+    INDEX `appointment_id_idx` (`appointment_id` ASC) VISIBLE,
+    CONSTRAINT `appointment_id` FOREIGN KEY (`appointment_id`) REFERENCES `appointment_db`.`appointments` (`appointment_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE = InnoDB;
