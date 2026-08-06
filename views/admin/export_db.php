05dff949837dbce2d6725e92c27e80749078bf9c
Atomic <anthony.namol.090105@gmail.com>
2026-08-06 15:07:19 +0800

    build: Clarify file structure

--- views/admin/export_db.php
+++ views/admin/export_db.php
@@ -5,7 +5,6 @@ require_once '../db.php';
 header('Content-Type: application/json');
 header('Content-Disposition: attachment; filename="appointment_db_export_' . date('Y-m-d_H-i') . '.json"');
 
-
 $tables = ['users', 'doctors', 'appointments', 'logs'];
 $backup = [];
 
