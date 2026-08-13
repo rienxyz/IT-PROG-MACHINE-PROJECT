f88779d36ffacf3a219993c30410c8e366e979ad
Atomic <anthony.namol.090105@gmail.com>
2026-08-13 22:48:28 +0800

    feat: Add bootstrap cdn

--- sign_in.php
+++ sign_in.php
@@ -30,9 +30,15 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous" defer></script>
 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <title> MLS · Sign In </title>
 </head>
+('System', 'Administrator', '+63 999-999-9999', 'admin@hospital.com', 'admin123', 'admin', 'active', 'verified');
 
 <body>
     <h2> Sign In </h2>
--- sign_up.php
+++ sign_up.php
@@ -45,14 +45,19 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous" defer></script>
 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <title> MLS · Sign Up </title>
 </head>
 
 <body>
-    <h1> Sign Up </h1>
+    <h2> Sign Up </h2>
 
     <div>
         <?php echo $error ?: $success ?>
